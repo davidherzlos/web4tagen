661c256e9653d30e6259e472c044ac2c544ea851
davidherzlos <davidherzlos@gmail.com>
2014-04-07 19:45:36 -0500

    Agregados APuntes

---
+++ Archivos de clase/eventos.js
@@ -0,0 +1,24 @@
+Los frameworks o librerías facilitan las diferencias entre navegadores
+
+
+// Algunos datos de JS, previos a jquery
+
+// Todos necesitan comillas en los parámetros
+
+getElementsByTagName('') // Nos retorma una etiqueta
+getElementById('') // Nos retorna el id que especifiquemos
+getElementsByClassName('') // Nos retorna una clase que especifiquemos
+querySelectorAll('') // Obtener todos los elementos que cumplen con un selector css
+querySelector('') // Obtener solo un elemento que cumple con un selector css
+
+
+// EVENTOS
+// Es cualquier interacción
+// Existen estándares para trabajar con event listeners
+// Notación: element.addEventListener("click", funcion)
+
+// IE old style : element.attachEvent("onClick", funcion)
+
+// Las librerías facilitan las diferencias, jquery, mootools, dogo.
+
+//J query
